ec2c32edf229da1648ef849e5342781a2fb7d068
SayantanScience <62644027+SayantanScience@users.noreply.github.com>
2021-05-31 16:42:09 +0530

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sushanta
+ */
+public class Config {
+    
+    protected static final int CoordinateX=500;
+    protected static final int CoordinateY=500;
+    protected static final int NUMBER_OF_CITIES=113;
+    protected static final int DELAY_MILLIS=500;
+    protected static final int DELAY_IN_SAME=10;
+    protected static final int MAX_CITIES=13;
+    
+}
